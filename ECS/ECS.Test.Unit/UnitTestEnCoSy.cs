98db8268c31d3de80f7a15f8b0c70df8fb9aeffb
Bjarke <201805703@post.au.dk>
2021-02-18 14:40:09 +0100

    ECS tests done

--- ECS/ECS.Test.Unit/UnitTestEnCoSy.cs
+++ ECS/ECS.Test.Unit/UnitTestEnCoSy.cs
@@ -1,12 +1,17 @@
 using NUnit.Framework;
-
+using NSubstitute;
 namespace ECS.Test.Unit
 {
     public class StubHeater : IHeater
     {
+        private bool _result;
+        public StubHeater(bool SelfResult =true)
+        {
+            _result = SelfResult;
+        }
         public bool RunSelfTest()
         {
-            return true;
+            return _result;
         }
 
         public void TurnOff()
@@ -21,9 +26,12 @@ namespace ECS.Test.Unit
     public class StubTempSensor : ITempSensor
     {
         private int _temp;
-        public StubTempSensor(int temp)
+        private bool _result;
+
+        public StubTempSensor(int temp=0,bool SelfResult =true)
         {
             _temp = temp;
+            _result = SelfResult;
         }
 
         public int GetTemp()
@@ -33,19 +41,22 @@ namespace ECS.Test.Unit
 
         public bool RunSelfTest()
         {
-            return true;
+            return _result;
         }
     }
 
     public class Tests
     {
-        private IEnCoSy _myECS;
+        private EnCoSy _myECS;
         private IHeater _myHeater;
         private ITempSensor _myTempSensor;
         [SetUp]
         public void Setup()
         {
             _myHeater = new StubHeater();
+            _myTempSensor = new StubTempSensor();
+            _myECS = new EnCoSy(25, _myTempSensor, _myHeater);
+
         }
 
         [TestCase(10)]
@@ -60,47 +71,60 @@ namespace ECS.Test.Unit
             Assert.That(_myECS.GetThreshold(),Is.EqualTo(thr));
         }
 
-        [TestCase(10)]
-        [TestCase(0)]
-        [TestCase(-10)]
-        public void RunSelfTestReturnsCorrect(int temp)
+        [TestCase(false,false,Description = "Both returns false")]
+        [TestCase(true,false,Description = "Sensor is true and Heater is false")]
+        [TestCase(false,true,Description = "Sensor is false and Heater is true")]
+        [TestCase(true,true,Description = "Both returns true")]
+        public void RunSelfTestReturnsCorrect(bool SensorRes, bool heatTest)
         {
             //Arange
-            _myTempSensor = new StubTempSensor(temp);
-
+            _myHeater = new StubHeater(heatTest);
+            _myTempSensor = new StubTempSensor(0,SensorRes);
             _myECS = new EnCoSy(1, _myTempSensor, _myHeater);
 
             //Action
-            bool ok = _myECS.RunSelfTest();
-
-            bool sensOk = _myTempSensor.RunSelfTest();
-            bool heatOK = _myHeater.RunSelfTest();
-            bool depOK = sensOk && heatOK;
-
-            Assert.That(ok, Is.EqualTo(depOK));
+            //Assert
+            Assert.That(_myECS.RunSelfTest(), Is.EqualTo(SensorRes && heatTest));
         }
 
-        [TestCase(1)]
-        [TestCase(0)]
-        [TestCase(-30)]
-        public void GetCurTempReturnsCurTemp(int temp)
+        [TestCase(1,1)]
+        [TestCase(1,0)]
+        [TestCase(1,-30)]
+        public void GetCurTempReturnsCurTemp(int thr, int temp)
         {
             _myTempSensor = new StubTempSensor(temp);
+            _myECS = new EnCoSy(thr, _myTempSensor, _myHeater);
             Assert.That(_myECS.GetCurTemp(), Is.EqualTo(temp));
         }
 
-        [TestCase(5,10,true)]
+        [TestCase(5,6,true,Description = "Positive Thr higher then positive temp")]
+        [TestCase(-5, -10, false, Description = "Negative numbers thr bellow temp")]
+        [TestCase(5, 5, false, Description = "Equel temp and thr")]
+        [TestCase(11, 10, false, Description = "Temp higher then thr")]
+        [TestCase(-5, 10, true,Description = "Negative temp and positive thr")]
         public void RegulateRegulaesCorrect(int temp,int thr,bool On_off)
         {
             //Arrange
+            var _dummyHeater = Substitute.For<IHeater>();
+
             _myTempSensor = new StubTempSensor(temp);
+
+            _myECS = new EnCoSy(thr, _myTempSensor, _dummyHeater);
             _myECS.SetThreshold(thr);
 
             //Action
             _myECS.Regulate();
 
             //Assert
-            Assert.That(_myECS._Heater.Received().TurnOn(), Is.EqualTo(On_off));
+            if(On_off)
+            {
+                _dummyHeater.Received().TurnOn();
+            }
+            else
+            {
+                _dummyHeater.Received().TurnOff();
+            }
+            
         }
 
         [TearDown]
